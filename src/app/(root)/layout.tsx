a75d36e10e5d0f6ab9bd4448478d5cfeb8aba508
Jason Connolly <jcodog@jconet.co.uk>
2025-07-22 14:32:24 +0100

    feat: Remove scrollbar from landing page using tailwindcss utility and webkit

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
 	// Remove default padding for full-bleed landing background
-	return <main className="flex flex-1 flex-col">{children}</main>;
+	return <main className="flex flex-1 flex-col no-scrollbar">{children}</main>;
 };
 
 export default Layout;
