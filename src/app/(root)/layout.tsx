bdba3e3795bcec6e17bb6ea5d7494ae7febd256b
Jason Connolly <jcodog@jconet.co.uk>
2025-09-24 23:15:44 +0100

    fix: unable to scroll landing page

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-	// Remove default padding for full-bleed landing background
-	return <main className="flex flex-1 flex-col no-scrollbar">{children}</main>;
+  // Remove default padding for full-bleed landing background
+  return (
+    <main className="flex flex-1 flex-col no-scrollbar overflow-auto">
+      {children}
+    </main>
+  );
 };
 
 export default Layout;
