1b18a0eb4f39de2cb8df37c7ea367ac026325566
Jason Connolly <jcodog@jconet.co.uk>
2026-01-07 19:17:26 +0000

    chore: bump stripe api version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -11,7 +11,7 @@ export const loadStripe = ({ secretKey }: { secretKey: string }) => {
   }
   if (!globalThis.stripeClients[secretKey]) {
     globalThis.stripeClients[secretKey] = new Stripe(secretKey, {
-      apiVersion: "2025-11-17.clover",
+      apiVersion: "2025-12-15.clover",
       typescript: true,
     });
   }
